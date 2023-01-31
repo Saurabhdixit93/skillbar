d9b3f2177068f400126706839fffc4c57a960bd0
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2023-01-31 15:05:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,26 @@
   button.addEventListener("click", function(){
     alert("Thanks For Rating.....");
   });
+
+
+ // PreLoader For web
+  window.addEventListener("load", function() {
+      setTimeout(function (){
+        const preloader = document.querySelector(".preloader");
+        preloader.style.display = "none";
+      },2000);// 2000ms = 2 seconds
+  });
+  
+  window.addEventListener("load", function() {
+      setTimeout(function (){
+        document.getElementById("spinner").style.display = "none";
+      },2000);// 2000ms = 2 seconds
+  });
+
+  window.addEventListener("beforeunload", function() {
+      setTimeout(function (){
+          document.getElementById("spinner").style.display = "block";
+      },2000); // 2000ms = 2 seconds
+  });
+
   
